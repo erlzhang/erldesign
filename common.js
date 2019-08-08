8904a33c38b3055594acc4f78fdead8add8054e9
Sharon Zhang <zhangshiyu1992@hotmail.com>
2019-08-08 17:10:25 +0800

    Add files via upload

--- common.js
+++ common.js
@@ -1,28 +1,96 @@
-var name = document.getElementsByClassName('author_name')[0].innerHTML;
-var name_content = name.innerHTML;
 
-var con = document.getElementsByClassName('author_name')[0];
-con.innerHTML = name[0];
+var feature = getSection('feature'),
+	style = getSection('style'),
+	project = getSection('project'),
+	skill = getSection('skills'),
+	contact = getSection('contact');
 
-var i = 1;
 
-if( i <= name.length){
-	var increase = setInterval(function(){
-		var s = con.innerHTML;
-		con.innerHTML = s + name[i];
-		console.log(i);
-		if( i >= (name.length-1) ){
-			clearInterval(increase);
-			document.getElementsByClassName('career')[0].style.opacity = "1";
-			setTimeout(function(){
-				document.getElementsByClassName('career-after')[0].style.width = "100%";
-				document.getElementsByClassName('second')[0].style.opacity = "1";
-			},400);
-			setTimeout(function(){
-				document.getElementsByClassName('more')[0].style.opacity = "1";
-			},900);
-		}
-		i++;
-	},150);
+var windowHeight = window.innerHeight,offset = 200;
+
+document.body.onscroll = function(){
+	var top = document.body.scrollTop;
+	
+	if( top >= (feature.offsetTop - windowHeight + 200) && feature.in == 0 ){
+		addClass(feature,'in');
+		feature.in = 1;
+	}
+	
+	if( top >= (style.offsetTop - windowHeight + 200) && style.in == 0 ){
+		addClass(style,'in');
+		style.in = 1;
+	}
+	
+	if( top >= (project.offsetTop - windowHeight + 200) && project.in == 0 ){
+		addClass(project,'in');
+		project.in = 1;
+	}
+	
+	if( top >= (skill.offsetTop - windowHeight + 200) && skill.in == 0 ){
+		addClass(skill,'in');
+		skill.in = 1;
+		addClass(contact,'in');
+		contact.in = 1;
+	}
 }
 
+var container = document.getElementsByClassName('features')[0];
+//var containerHeight = document.defaultView.getComputedStyle(container).height;
+var featureList = document.getElementsByClassName('feature_item');
+var featureNav = document.getElementsByClassName('feature-nav-item');
+container.style.position = 'relative';
+//container.style.height = containerHeight;
+var featureIndex = 0;
+showFeature(featureIndex);
+
+setInterval(nextFeature,5000);
+
+document.getElementsByClassName('feature-next')[0].onclick = nextFeature;
+
+
+
+function nextFeature(){
+	if( featureIndex == (featureList.length - 1) ){
+		featureIndex = 0;
+	}else{
+		featureIndex++;
+	}
+	showFeature(featureIndex);
+}
+
+function showFeature(index){
+	featureIndex = index;
+	for ( var i = 0,l = featureList.length ; i < l; i++ ){
+		featureList[i].style.position = 'absolute';
+		if( index == i){
+			featureList[i].style.opacity = 1;
+			featureList[i].style.zIndex = 3;
+			addClass(featureNav[i],"c");
+		}else{
+			featureList[i].style.opacity = 0;
+			featureList[i].style.zIndex = 2;
+			featureList[i].style.display = "block";
+			removeClass(featureNav[i],"c");
+		}
+	}	
+}
+
+function getSection(className){
+	var e = document.getElementsByClassName(className)[0];
+	e.in = 0;
+	return e;
+}
+
+function addClass(e,c){
+	var o = e.className;
+	if(o && o[o.length -1]!= " " ){
+		o = o + " ";
+	}
+	e.className = o + c;
+}
+
+function removeClass(e,c){
+	var pattern = new RegExp("\\b" + c + "\\b\\s*","g");
+	var o = e.className;
+	e.className = o.replace(pattern,"");
+}
